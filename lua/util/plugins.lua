e2d7f31ecba1f5eab9f1e22d4dc0227cf9b6971b
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-23 14:39:28 -0500

    Add helper

--- lua/util/plugins.lua
+++ lua/util/plugins.lua
@@ -15,4 +15,14 @@ function M.opts(name)
   return Plugin.values(plugin, "opts", false)
 end
 
+---@param fn fun()
+function M.on_very_lazy(fn)
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+      fn()
+    end,
+  })
+end
+
 return M
