d6daf4626eb5d6201789c382ac79411a12b89f08
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-22 23:18:18 -0500

    Added lazygit

--- lua/specs/tools/terminal.lua
+++ lua/specs/tools/terminal.lua
@@ -74,6 +74,33 @@ return {
         end
       end,
     },
-    config = true,
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+      local Terminal  = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "double",
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      vim.api.nvim_set_keymap("n", "<Space>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+    end,
   },
 }
+
